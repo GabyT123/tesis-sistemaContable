94f60a10ffa942692f142e410aa9e3b21b5035cc
GabyT123 <dgtorres9@utpl.edu.ec>
2024-07-17 22:36:38 -0500

    prueba de integracion continua

--- pages/ventas.tsx
+++ pages/ventas.tsx
@@ -4,7 +4,7 @@ import RoleLayout from "../lib/layouts/role_layout";
 const Ventas = () => {
   return (
     <>
-      <RoleLayout permissions={[0, 4, 5]}>
+      <RoleLayout permissions={[0, 4]}>
         <title>Ventas</title>
         <div className="flex h-screen">
           <div className="md:w-1/6 max-w-none">
